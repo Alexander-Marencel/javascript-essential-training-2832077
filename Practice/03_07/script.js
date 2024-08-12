e9091aac2a076249a1d2492044589ce5ee7e1d0f
Alex Marencel <alex.marencel@workvis.io>
2024-08-12 19:39:29 -0400

    Practice

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,20 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const phone = {
+    model: "Galaxy S-21 5G",
+    model_name: "SM-G991U",
+    apps: {
+        snapchat: {
+            username: "LFC_96",
+            snapscore: 124356
+        },
+        facebook: {
+            username: "The_Dude",
+        }
+    }
+
+};
+
+console.log("Model:", phone.model)
+console.log("Model Name:", phone["model_name"])
