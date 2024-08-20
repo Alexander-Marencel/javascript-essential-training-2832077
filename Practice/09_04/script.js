f2328b4ae0919437b07eb708221c39be7b5ae5d5
Alex Marencel <alex.marencel@workvis.io>
2024-08-20 19:27:36 -0400

    practice with event listeners

--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,68 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+const entireGridOutline = function (){
+    let gridContainer = document.querySelector(".grid");
+    gridContainer.addEventListener("mouseover", () => {gridContainer.style.outline = "6px solid red"} );
+    gridContainer.addEventListener("mouseout", () => {gridContainer.style.outline = ""} );
+};
+
+const eachCellOutline = function (){
+    let gridContainer = document.querySelectorAll(".grid .cell");
+    for (let i = 0; i < gridContainer.length; i++) {
+        let cell = gridContainer[i];
+        cell.addEventListener("mouseover", () => {cell.style.outline = "6px solid red"} );
+        cell.addEventListener("mouseout", () => {cell.style.outline = ""} );
+    };
+};
+
+const eachCellBackground = function (){
+    let gridContainer = document.querySelectorAll(".grid .cell");
+    for (let i = 0; i < gridContainer.length; i++) {
+        let cell = gridContainer[i];
+        cell.addEventListener("click", () => {cell.style.backgroundColor = "red"} );
+        cell.addEventListener("dblclick", () => {cell.style.backgroundColor = ""} );
+    };
+};
+
+const dim = function(event){
+    if (event.key === "-"){
+        console.log(event.key)
+    }
+
+}
+
+const changeBackground = function(){
+    let bodyContainer = document.querySelector("body");
+    hue = 0;
+    sat = 100;
+    light = 100;
+    bodyContainer.addEventListener("keypress", (event) => {
+        if ((event.key === "-" || event.key === "_") && light > 0){
+            light--;
+            bodyContainer.style.backgroundColor = "hsl("+hue+","+sat+"% ,"+light+"%)";
+            console.log("Light value:", light)
+        }
+
+        if ((event.key === "+" || event.key === "=") && light < 100){
+            light++;
+            bodyContainer.style.backgroundColor = "hsl("+hue+","+sat+"% ,"+light+"%)";
+            console.log("Light value:", light)
+        }
+
+        if (event.key === "h" || event.key === "H"){
+            if (hue == 356){hue=0} 
+            hue++;
+            bodyContainer.style.backgroundColor = "hsl("+hue+","+sat+"% ,"+light+"%)";
+            console.log("Hue value:", hue)
+        }
+    
+    })
+
+};
+
+entireGridOutline();
+eachCellOutline();
+eachCellBackground();
+changeBackground();
